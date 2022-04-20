8e99b22c698e96373629a8dba7c4b89d0543bd30
Isi Zahidli <ismayil.zahidli@mail.ru>
2022-04-20 22:11:02 +0400

    Add models, mappers of 3 entity

---
+++ Library/Mappers/EmployeeMapper.cs
@@ -0,0 +1,53 @@
+﻿using Library.Models;
+using LibraryCore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Mappers
+{
+    public class EmployeeMapper
+    {
+        public static EmployeeModel Map(Employee employee)
+        {
+            var model = new EmployeeModel();
+
+            model.Id = employee.Id;
+            model.Name = employee.Name;
+            model.PhoneNumber = employee.PhoneNumber;
+            model.Surname = employee.Surname;
+            model.Address = employee.Address;
+            model.BeginWorkTime = employee.BeginWorkTime;
+            model.BirthDate = employee.BirthDate;
+            model.EndWorkTime = employee.EndWorkTime;
+            model.Gender = employee.Gender;
+            model.Salary = employee.Salary;
+            model.JoiningDate = employee.JoiningDate;
+            model.Department = DepartmentMapper.Map(employee.Department);
+
+            return model;
+        }
+
+        public static Employee Map(EmployeeModel model)
+        {
+            Employee destination = new Employee();
+
+            destination.Id = model.Id;
+            destination.Name = model.Name;
+            destination.PhoneNumber = model.PhoneNumber;
+            destination.Surname = model.Surname;
+            destination.Address = model.Address;
+            destination.BeginWorkTime = model.BeginWorkTime;
+            destination.BirthDate = model.BirthDate;
+            destination.EndWorkTime = model.EndWorkTime;
+            destination.Gender = model.Gender;
+            destination.Salary = model.Salary;
+            destination.JoiningDate = model.JoiningDate;
+            destination.Department = DepartmentMapper.Map(model.Department);
+
+            return destination;
+        }
+    }
+}
---
+++ Library/Models/EmployeeModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class EmployeeModel : BaseModel
+    {
+        public override object this[string propName]
+        {
+            get
+            {
+                if (this == null) return null;
+
+                var type = typeof(EmployeeModel);
+                var props = type.GetProperties();
+                var prop = props.FirstOrDefault(x => x.Name == propName);
+
+                if (prop == null)
+                    return null;
+
+                return prop.GetValue(this);
+            }
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool Gender { get; set; }
+        public DepartmentModel Department { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public decimal Salary { get; set; }
+        public TimeSpan BeginWorkTime { get; set; }
+        public TimeSpan EndWorkTime { get; set; }
+
+        public bool Contains(string searchText)
+        {
+            var lowerText = searchText.ToLower(); // or ToUpper()
+
+            return (Name != null && Name.ToLower().Contains(lowerText)) ||
+                    (Surname != null && Surname.ToLower().Contains(lowerText)) ||
+                    (PhoneNumber != null && PhoneNumber.ToLower().Contains(lowerText)) ||
+                    (Address != null && Address.ToLower().Contains(lowerText)) ||
+                    Department.Contains(lowerText);
+        }
+    }
+}
